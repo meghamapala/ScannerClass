9768560996ec1267635b8a073a61919923730239
Mapala1965 <mapalamegha@gmail.com>
2021-10-01 08:46:09 -0400

    first commit

---
+++ src/fibonaccinumberproject/FibonacciNumber.java
@@ -0,0 +1,19 @@
+package fibonaccinumberproject;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int age=scanner.nextInt();
+		System.out.println(age);
+		
+		String name=scanner.nextLine();
+		System.out.println(name);
+
+	}
+
+}
